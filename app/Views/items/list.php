c630a982572d412f4ca91f42d64019d7641dcf9f
jaypee-cmyk <janpatrickabayon@gmail.com>
2026-05-04 19:03:42 +0800

    Ensure Burger Patty quantity and pieces are inline on a single line

--- app/Views/items/list.php
+++ app/Views/items/list.php
@@ -937,12 +937,7 @@ function getSku($name, $variation = '') {
                             <td class="text-center align-middle"><?= esc($item['name']) ?></td>
                             <td class="text-center align-middle"><?= esc(getSku($item['name'])) ?></td>
                             <td class="text-center align-middle">₱<?= number_format($item['price'], 2) ?></td>
-                            <td class="text-center align-middle">
-                                <span><?= esc($item['quantity']) ?></span>
-                                <?php if (stripos($item['name'], 'burger patty') !== false): ?>
-                                    <small class="text-muted ms-1">(6)</small>
-                                <?php endif; ?>
-                            </td>
+                            <td class="text-center align-middle"><span><?= esc($item['quantity']) ?></span><?php if (stripos($item['name'], 'burger patty') !== false): ?> <small class="text-muted">(6)</small><?php endif; ?></td>
                             <td class="text-center align-middle hide-mobile">
                                 <?php 
                                 $val = $item['price'] * $item['quantity'];
